e65f65e2edbf70a88618b3d9ca7b52b947cba920
Mircea Mazilu <mazilumircea11@gmail.com>
2023-05-30 21:52:23 +0300

    Added adress

--- PizzaDelivery/PizzaDelivery/Entities/Adresa.cs
+++ PizzaDelivery/PizzaDelivery/Entities/Adresa.cs
@@ -6,7 +6,52 @@ using System.Threading.Tasks;
 
 namespace PizzaDelivery.Entities
 {
-    internal class Adresa
+    internal class Adresa : ICloneable
     {
+        private String _strada;
+        private String _numar;
+        private String _cartier;
+
+        public Adresa(string strada, string numar, string cartier)
+        {
+            _strada = strada;
+            _numar = numar;
+            _cartier = cartier;
+        }
+
+        public String strada { get; set; }
+        public String numar { get; set; }
+        public string cartier { get; set; }
+
+        public static Adresa operator ++(Adresa adresa)
+        {
+            int numarStrada = int.Parse(adresa.numar);
+            numarStrada++;
+            adresa.numar = numarStrada.ToString();
+            return adresa;
+        }
+
+        public static Adresa operator --(Adresa adresa)
+        {
+            int numarStrada = int.Parse(adresa.numar);
+            numarStrada--;
+            adresa.numar = numarStrada.ToString();
+            return adresa;
+        }
+
+        public override string ToString()
+        {
+            return $"Strada: {_strada}\n Numar: {_numar}\n Cartier: {_cartier}";
+        }
+
+        public object Clone()
+        {
+            return new Adresa(_strada, _numar, _cartier);
+        }
+
+        public void adaugaStr(Adresa adresa)
+        {
+            String newStrada = "Str." + adresa.strada;
+        }
     }
 }
